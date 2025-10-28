f2cf170317dda95a6951827c75ebe16856581de6
nahu <namhn.haui@gmail.com>
2025-10-28 17:11:46 +0700

    nahu

---
+++ styles/init.js
@@ -0,0 +1,29 @@
+import 'https://nhatmai8112025.iwedding.info/common/emoji-picker/js/index.js';
+import 'https://nhatmai8112025.iwedding.info/common/emoji-picker/js/picker.js';
+import 'https://nhatmai8112025.iwedding.info/common/emoji-picker/js/database.js';
+
+$(".textarea-emoji-picker emoji-picker").dataSource = 'https://nhatmai8112025.iwedding.info/common/emoji-picker/js/data.json';
+$(document).ready(function() {
+    $(document).on('emoji-click', '.textarea-emoji-picker emoji-picker', function(e){
+        let formInput = $(this).parents('.textarea-emoji-picker').find("textarea");
+        let submitButton = $(this).data('button');
+        insertTextAtCursor(formInput, e.detail.unicode);
+        if(typeof submitButton !== 'undefined') {
+            $('.btnSavePost').removeAttr('disabled');
+        }
+    });
+    
+    $(document).on('click', '.emoji-picker-button', function(e) {
+        var _this = $(this).parents('.textarea-emoji-picker').find(".emoji-picker");
+        $(".emoji-picker").not(_this).hide();
+        _this.toggle();
+    });
+    
+    $(document).on('click', 'emoji-picker, .emoji-picker, .emoji-picker-button', function(e) {
+        e.stopPropagation();
+    });
+    
+    $(document).click(function(){
+        $(".emoji-picker").hide();
+    });
+});
